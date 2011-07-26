e0ec9edbb9488290dbc06ab012ef2b31f31e898d
jsimons (jack simons) <jcsimons@verizon.net>
2011-07-26 02:44:20 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cans-works@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/function str.r
@@ -0,0 +1,12 @@
+# -----------------------------------------------------------------------------
+# str() with list length = 300
+# -----------------------------------------------------------------------------
+# Jack Simons <jack.simons@state.ma.us> 
+# Monday July 25, 2011; 22:42:55
+
+# Supplies parameter so str() will give long enough (300) list to examine 
+# 254 column cans dataframe. 
+
+str <- function(x) {
+	str(x, list.length = 300)
+	}
