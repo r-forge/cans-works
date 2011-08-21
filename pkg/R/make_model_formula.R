9e4356a1f5cadd5e35bb8ed37d901755c2a36f2c
jsimons (jack simons) <jcsimons@verizon.net>
2011-08-21 00:31:57 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cans-works@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/make_model_formula.R
+++ pkg/R/make_model_formula.R
@@ -1,5 +1,14 @@
 make_model_formula <-
 function(leftvars=0, rightvars=0, data) {
+
+# Jack Simons <jack.simons@state.ma.us>
+# 2:54 PM Tuesday, July 05, 2011
+
+# create a simple model formula using large number of variables
+# Constructs a model formula based on two vectors, which contain the indices of the dataframe columns to be selected in the model.
+# Note that the formula uses the + operator for all variables; user must make any needed edits manually.
+# To make either side of the formula blank, use 0 for the index value.
+
 leftstring <- paste(names(data)[leftvars], collapse=" + ")
 # print(c("leftvars:", paste(leftvars, collapse=" ")))
 # print(c("leftstring:", leftstring))
@@ -8,4 +17,3 @@ rightstring <- paste(names(data)[rightvars], collapse=" + ")
 # print(c("rightstring:", rightstring))
 model<-paste(leftstring, " ~ ", rightstring)
 model}
-
