9b05192238211378a57448f5ca3d21b7d1352e5e
jsimons (jack simons) <jcsimons@verizon.net>
2011-08-21 00:23:59 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cans-works@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/str.R
+++ pkg/R/str.R
@@ -1,3 +1,10 @@
-str <-
-function (object, ...) 
-UseMethod("str")
+str <- function(x) {
+
+# Jack Simons <jack.simons@state.ma.us> 
+# Monday July 25, 2011; 22:42:55
+
+# Supplies parameter so str() will give long enough (300) list to examine 
+# 254 column cans dataframe. 
+
+	str(x, list.len = 300)
+	}
