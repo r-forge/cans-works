592a8ecf9fc478ba2204f6e578a1f19a9afa23d6
jsimons (jack simons) <jcsimons@verizon.net>
2011-08-21 00:26:57 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cans-works@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/stamp.R
+++ pkg/R/stamp.R
@@ -1,5 +1,11 @@
 stamp <-
 function (author = 'Jack Simons <jack.simons@state.ma.us>') {
+
+# Jack Simons <jack.simons@state.ma.us>
+# 2:54 PM Tuesday, July 05, 2011
+# gives author contact info and datestamp
+# Handy when updating metadata in scripts and functions.
+# Parameter 'author' is a string with author name and email.
+
   cat(author, " \n", format(Sys.time(), "%A %B %d, %Y; %H:%M:%S"), sep='')
   }
-
