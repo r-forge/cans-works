424209ca322f4abc517631542754fd8e62e56ff5
jsimons (jack simons) <jcsimons@verizon.net>
2011-08-21 23:37:15 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cans-works@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/get_one_CANS_per_person.R
+++ pkg/R/get_one_CANS_per_person.R
@@ -1,5 +1,14 @@
 get_one_CANS_per_person <-
 function  (x, most_recent = TRUE) {
+
+# gets one CANS per person
+# jack.simons@state.ma.us
+# Thursday June 23, 2011; 16:58:30
+
+# Selects either the newest or oldest CANS for every person
+
+# todo: Build in a check to confirm that column 32 is a date and that column 2 is a factor.
+
 # first get order by date, either increasing or decreasing
   date.order <- order(x[, 32], decreasing=most_recent)
 # now get order by MHID
