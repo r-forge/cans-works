b74fd0e81ef0360a59525d48f9efac575a60a6f7
jsimons (jack simons) <jcsimons@verizon.net>
2011-09-12 18:53:50 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cans-works@66 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/select_persons.r
@@ -0,0 +1,19 @@
+select_persons <- function (cans, observations, person.id = 2) {
+
+# Select persons by number of observations
+
+# Select the subset of persons who have a specified number 
+# of observations in the CANS dataframe
+
+# cans is a CANS dataframe
+# person.id is an integer, the column number of the person identifier
+# observations is a vector of integers which are the desired number
+# of observations; could be a single integer, a range, or any set of values
+
+# Returns a dataframe where the first column is a character vector of person
+# identifiers, and the second column is the number of observatrions associated
+# with each person. Ordered by decreasing number of observations.
+
+persons <- as.data.frame(table(cans[, person.id]))
+persons <- persons[rev(order(persons[, 2])), ]
+}
