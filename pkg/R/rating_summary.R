85eee6a70e4cd07b35336a9f26cf6dd944d99222
jsimons (jack simons) <jcsimons@verizon.net>
2011-10-14 02:39:17 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cans-works@72 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rating_summary.R
+++ pkg/R/rating_summary.R
@@ -1,5 +1,5 @@
 rating.summary <-
-function(cans, obs.id = 1, person.id = 2, obs.date = 32, items = 54:64) {
+function(cans, obs.id = 1, person.id = 2, obs.date = 32, items = c(54:64, 66:75, 77:88) {
 
 # Summarizes ratings for selected group of items, record by record.
 # Computes the number of items with values of 0,1,2,3, and 
@@ -19,9 +19,9 @@ ones <- rowSums(cans[,items] == 1, na.rm=TRUE)
 twos <- rowSums(cans[,items] == 2, na.rm=TRUE)
 threes <- rowSums(cans[,items] == 3, na.rm=TRUE)
 NAs <- rowSums(is.na(cans[,items]))
-act.ratio <- round((twos+threes)/(zeros+ones+twos+threes),2)
+act.ratio <- (twos+threes)/(zeros+ones+twos+threes)
 all.sum <- sum(zeros, ones, twos, threes, NAs)
-means <- round(rowMeans(cans[,items], na.rm = TRUE), digits = 2)*10
+means <- rowMeans(cans[,items], na.rm = TRUE)*10
 
 # Create and return a dataframe of these values.
 data.frame(person, observation, date, zeros, ones, twos, threes, NAs, all.sum, act.ratio, means)
