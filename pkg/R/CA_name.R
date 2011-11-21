8b87073b195924dda9d82ccc97f9c32f3f7462ec
jsimons (jack simons) <jcsimons@verizon.net>
2011-11-21 04:59:21 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cans-works@86 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/CA_name.R
+++ pkg/R/CA_name.R
@@ -1,10 +1,21 @@
-CA_name <-
-function(x, show.name=TRUE, show.org=TRUE, 
-                     firstname.col = 3, lastname.col = 4, org.col = 5) {
+CA_name <-function
+###	Creates a single character string with assessor firstname, lastname and organization
+(x,
+###	the CANS dataframe 
+show.name=TRUE,
+### logical, whether to include name
+show.org=TRUE,
+###	logical, whether to include org
+firstname.col = 3,
+### numeric, column index for lastname
+org.col = 5
+### numeric, column index for organization
+){
 #   trims the white space after firstname and pastes with lastname
   if (show.name) x$CA <- paste(gsub(" +", "", x[, firstname.col]), x[, lastname.col], sep=' ')
 #   paste the org onto the name, with comma separator
   if (show.org) x$CA <- paste(x$CA, x[, org.col], sep=', ')
   x$CA
+###	returns a vector of length=nrow of original dataframe
   }
 
